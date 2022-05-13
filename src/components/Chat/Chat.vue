705858f1d87a9ad78a3f5dec95f5e7203cd4bedc
Luke Bouch <luke@lukebouch.com>
2022-05-12 22:05:08 -0400

    WIP

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -1,22 +1,28 @@
 <script>
+import { useChatStore } from "../../stores/chat"
 import Button from "../Button.vue"
 import ChooseChat from "./ChooseChat.vue"
 
 export default {
     components: { Button, ChooseChat },
+    setup() {
+        const chat = useChatStore();
+    },
     data() {
-        
+        return {
+            code: chat.code,
+        }
     }
 }
 </script>
 
 <template>
     <div class="container px-5">
-        <div v-if="true">
+        <div v-if="false">
             <ChooseChat></ChooseChat>
         </div>
         <div v-else>
-        
+        Test
         </div>
     </div>
 </template>
