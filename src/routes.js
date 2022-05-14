733cbeda24c589d75a67e48abb149b92f1baf550
Luke Bouch <luke@lukebouch.com>
2022-05-13 22:04:42 -0400

    Nuke 🧨

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import Landing from "./components/Landing.vue"
-import Chat from "./components/Chat/Chat.vue"
-import ChooseChat from "./components/Chat/ChooseChat.vue"
-
-export default [
-    { path: '/', component: Landing },
-    { path: '/chat', component: Chat },
-    { path: '/chat/choose', component: ChooseChat },
-]
