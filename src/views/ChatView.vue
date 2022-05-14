f881f0878fae759504b303b8e5bf1f042efa8936
Luke Bouch <luke@lukebouch.com>
2022-05-13 22:43:13 -0400

    WIP

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -1,10 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            code: null
+        }
+    }
+}
+</script>
+
 <template>
     <div class="flex flex-col justify-between h-48 gap-10 px-5 my-10 text-center container-sm">
         <div>
             <h1>Join a Chat</h1>
             <div class="mb-5">
                 <input class="w-full px-2 py-3 text-5xl font-bold tracking-widest text-center border-2 rounded-xl"
-                    type="number" placeholder="00000">
+                    v-model="code" type="number" placeholder="00000">
             </div>
             <Button>Join Chat</Button>
         </div>
