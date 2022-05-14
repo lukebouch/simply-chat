c48d436ddcefa198a7f5424ffddca4e0bf1c8c2d
Luke Bouch <luke@lukebouch.com>
2022-05-13 22:46:29 -0400

    WIP

--- src/views/ChatView.vue
+++ src/views/ChatView.vue
@@ -2,6 +2,7 @@
 export default {
     data() {
         return {
+            enteredCode: null,
             code: null
         }
     }
@@ -9,19 +10,26 @@ export default {
 </script>
 
 <template>
-    <div class="flex flex-col justify-between h-48 gap-10 px-5 my-10 text-center container-sm">
-        <div>
-            <h1>Join a Chat</h1>
-            <div class="mb-5">
-                <input class="w-full px-2 py-3 text-5xl font-bold tracking-widest text-center border-2 rounded-xl"
-                    v-model="code" type="number" placeholder="00000">
-            </div>
-            <Button>Join Chat</Button>
+    <div v-if="code">
+        <div class="flex flex-col justify-between h-48 gap-10 px-5 my-10 text-center container-sm">
+            <span class="text-xl">Chat code: <span class="font-bold">{{ code }}</span></span>
         </div>
-        <span class="text-xl">or</span>
-        <div>
-            <h1>Start a new one</h1>
-            <Button>New Chat</Button>
+    </div>
+    <div v-else>
+        <div class="flex flex-col justify-between h-48 gap-10 px-5 my-10 text-center container-sm">
+            <div>
+                <h1>Join a Chat</h1>
+                <div class="mb-5">
+                    <input class="w-full px-2 py-3 text-5xl font-bold tracking-widest text-center border-2 rounded-xl"
+                        v-model="enteredCode" type="number" placeholder="00000">
+                </div>
+                <Button @click="code = enteredCode">Join Chat</Button>
+            </div>
+            <span class="text-xl">or</span>
+            <div>
+                <h1>Start a new one</h1>
+                <Button>New Chat</Button>
+            </div>
         </div>
     </div>
 </template>
