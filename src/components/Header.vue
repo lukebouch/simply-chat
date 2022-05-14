0b29b165e509dd5bd5df1d06fb154e3b7f06fa1f
Luke Bouch <luke@lukebouch.com>
2022-05-13 22:27:30 -0400

    Add header

---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+        <div class="container flex items-center justify-center p-5">
+            <router-link to="/"><img class="h-16" src="../assets/logo.png" alt="Simply Chat logo"></router-link>
+        </div>
+    </header>
+</template>
